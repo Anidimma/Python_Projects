2b4ada0f7358cb0d8a232e756a3822a0bf9c2858
Anidimma <66795784+Anidimma@users.noreply.github.com>
2021-02-21 06:28:08 +0100

    Bug Fixes
    Fixed alot of issues in the code

--- Web Scraping with Python/Beautiful Soup/New_Port_Beach_Restaurants/WebScrapeRequestSoup.py
+++ Web Scraping with Python/Beautiful Soup/New_Port_Beach_Restaurants/WebScrapeRequestSoup.py
@@ -2,15 +2,12 @@
 #IMPORTING THE MODULES
 import requests
 from bs4 import *
-from docx import *
 import pandas as pd 
 
 
-columns = ['Name','Rating', 'Number','Location']
+columns = ['Name','Rating']
 hold = []
        
-general_doc = Document()
-images_doc = Document()
 
 # SPECIFYING HOW THE CODE SHOULD ACCESS THE WEBSITE
 """To understand how the variables under work with the code please
@@ -22,108 +19,72 @@ base_url = 'https://www.yelp.com/search?find_desc=Restaurants&find_loc=' # Initi
 loc = 'Newport Beach, CA' # Specifying the city
 sorter = 'recommended'
 current_page = 0
+count = 0
 
-"""The arrangement of the various code below is to beautify the arrangement on microsoft word"""
 
-all_values = general_doc.add_heading('THIS IS THE DATA SCRAPPED FROM YELP WEBSITE')
-all_values = general_doc.add_paragraph("""""")
+# GATHERING DATA FROM 200 SOURCES APART FROM IMAGE DATA 
 
-# GATHERING DATA FROM 200 SOURCES APART FROM IMAGE DATA   
-while current_page < 201:
 
-    url = base_url + loc + '&sortby='+ sorter + '&start=' + str(current_page)
-    yelp_r = requests.get(url)
-    yelp_soup = BeautifulSoup(yelp_r.text, 'html.parser')
+#Run the extraction multiple times, because the resturants displayed on each page change from time to time
+while count <= 5: 
+    while current_page < 201:
 
-    #SPECIFYING WHERE ON THE PAGE PYTHON SHOULD EXTRACT DATA FROM
-    general = yelp_soup.findAll(class_ ='arrange-unit__09f24__1gZC1 arrange-unit-fill__09f24__O6JFU border-color--default__09f24__R1nRO')
+        url = base_url + loc + '&sortby='+ sorter + '&start=' + str(current_page)
+        yelp_r = requests.get(url)
+        yelp_soup = BeautifulSoup(yelp_r.text, 'html.parser')
 
-    print('\n',f'You\'re on page {current_page}','\n')
+        #SPECIFYING WHERE ON THE PAGE PYTHON SHOULD EXTRACT DATA FROM
+        general = yelp_soup.findAll(class_ ='arrange-unit__09f24__1gZC1 arrange-unit-fill__09f24__O6JFU border-color--default__09f24__R1nRO')
 
-    for num,biz in enumerate(general):
-        if len(biz) > 1:
-            for num2,val in enumerate(biz):
-                if num2 == 0:
-                    val = str(val)
-                    holder = val.split('">(')
-                    if len(holder) == 2:
+        print('\n',f'You\'re on count {count} and on page {current_page}','\n')
+
+        for num,biz in enumerate(general):
+            if len(biz) > 1:
+                for num2,val in enumerate(biz):
+                    if num2 == 0:
+                        val = str(val)
+                        holder = val.split('">(')
+                        if len(holder) == 2:
+                            
+                            val1 = val.split('name=')[1].split('"')[1].replace('&amp;','&')  # Getting the name
+                            print(val1)
+
+                            if val.find('aria-label') == -1:
+                                val2 = 'NA'
+                            else:
+                                val2 = val.split('aria-label=')[1].split('"')[1]  # Getting the rating
+                            print(val2)
+
+                            container = [val1,val2]
+                            hold.append(container)
+                            print('\n')
                         
-                        val1 = val.split('name=')[1].split('"')[1].replace('&amp;','&')  # Getting the name
-                        print(val1)
-                        all_values.add_run(f""" 
+                        elif len(holder) == 1:
+                            val1 = val.split('name=')[1].split('"')[1].replace('&amp;','&')  # Getting the name
+                            print(val1)
 
-                        This is from page {current_page}  
-                        --------------------------------------------------------------------------------------------------------------
+                            if val.find('aria-label') == -1:
+                                val2 = 'NA'
+                            else:
+                                val2 = val.split('aria-label=')[1].split('"')[1]  # Getting the rating
+                            print(val2)
 
-                        {val1}""")
+                            container = [val1,val2]
+                            hold.append(container)
+                            print('\n')
 
-                        val2 = val.split('aria-label=')[1].split('"')[1]  # Getting the rating
-                        print(val2)
-                        all_values.add_run(f""" 
+        print('\n',f'You left count {count} and page {current_page}','\n')
 
-                        {val2}""")
+        current_page += 10
 
-                        val3 = val.split('">(')[1].split('</p></div>')[0]  # Getting the phone number
-                        val3 = '(' + val3 
-                        print(val3)
-                        all_values.add_run(f""" 
+    print('stop')
+    current_page = 0
+    count += 1
 
-                        {val3}""")
 
-                        val4 = val.split('buy">')[1].split('</span></p>')[0]  # Getting the location
-                        print(val4)
-                        all_values.add_run(f""" 
-
-                        {val4}
-
-                        """)
-
-                        container = [val1,val2,val3,val4]
-                        hold.append(container)
-                        print('\n')
-                    
-                    elif len(holder) == 1:
-                        val1 = val.split('name=')[1].split('"')[1].replace('&amp;','&')  # Getting the name
-                        print(val1)
-                        all_values.add_run(f""" 
-
-                        This is from page {current_page}  
-                        --------------------------------------------------------------------------------------------------------------
-
-                        {val1}""")
-
-                        val2 = val.split('aria-label=')[1].split('"')[1]  # Getting the rating
-                        print(val2)
-                        all_values.add_run(f""" 
-
-                        {val2}""")
-
-                        val3 = 'N/A'  # When no phone number available
-                        print(val3)
-                        all_values.add_run(f""" 
-
-                        {val3}""")
-                        
-                        val4 = val.split('buy">')[1].split('</span></p>')[0]  # Getting the location
-                        print(val4)
-                        all_values.add_run(f""" 
-
-                        {val4}
-
-                        """)
-
-                        container = [val1,val2,val3,val4]
-                        hold.append(container)
-                        print('\n')
-
-    general_doc.save(r'C:/Users/Divinity-PC/Desktop/practice/np_resturants.docx')  # Specifying the place where the data will be saved             
-    print('\n',f'You left page {current_page}','\n')
-
-    current_page += 10
-general_doc = Document() # Resetting the document place holder
-print('stop')
 
 df = pd.DataFrame(hold,columns = columns)
 df = df.drop_duplicates(subset= 'Name', keep="first", inplace=False, ignore_index = True) #Dropping Duplicate Entries 
 df.to_csv(r'C:/Users/Divinity-PC/Desktop/practice/np_resturants.csv', index=False)
+
 
